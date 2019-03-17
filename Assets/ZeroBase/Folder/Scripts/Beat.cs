98ec666a58b80b595392d89f9269fa292ac93b22
wj-choi <sq7r9760@naver.com>
2019-03-18 01:24:58 +0900

    Damaged effect added
    Damaged effect added

---
+++ Assets/ZeroBase/Folder/Scripts/Beat.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour
+{
+	[Header("Beat movement")]
+	public AnimationCurve defaultAnimCurve;
+	public AnimationCurve adrenalineAnimCurve;
+
+	[HideInInspector] public bool defaultTrigger;
+	[HideInInspector] public bool adrenalineTrigger;
+	float value;
+	float height;
+
+	void Start()
+	{
+		Invoke("DefaultMovement", 2f);
+	}
+
+	void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			defaultTrigger = true;
+			adrenalineTrigger = false;
+			value = 0;
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			adrenalineTrigger = true;
+			defaultTrigger = false;
+			value = 0;
+		}
+
+		if (adrenalineTrigger)
+		{
+			AdrenalineMovement();
+		}
+
+		else if (defaultTrigger)
+		{
+			DefaultMovement();
+		}
+
+		this.transform.position = new Vector3(this.transform.position.x, height * 4f, this.transform.position.z);
+	}
+
+	public void DefaultMovement()
+	{
+		defaultTrigger = true;
+		value += Time.deltaTime;
+		height = defaultAnimCurve.Evaluate(value);
+	}
+
+	public void AdrenalineMovement()
+	{
+		adrenalineTrigger = true;
+		value += Time.deltaTime;
+		height = adrenalineAnimCurve.Evaluate(value);
+	}
+}
---
+++ Assets/ZeroBase/Folder/Scripts/DamagedEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamagedEffect : MonoBehaviour
+{
+	Animator anim;
+	public Sprite[] sprites;
+	Image image;
+
+    void Awake()
+    {
+		anim = GetComponent<Animator>();
+		image = GetComponent<Image>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+		{
+			Hit();
+		}
+	}
+
+	void Hit()
+	{
+		int index = Random.Range(0, sprites.Length);
+		if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >0.75f)
+			image.sprite = sprites[index];
+		anim.SetTrigger("Hit");
+	}
+}
