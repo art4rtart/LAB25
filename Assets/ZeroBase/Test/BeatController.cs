0c0db1a5f721382e6e667cde63b14005b8069dd3
wj-choi <sq7r9760@naver.com>
2019-03-17 14:35:08 +0900

    Heartbeat renderer added
    Heartbeat renderer added

---
+++ Assets/ZeroBase/Test/BeatController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatController : MonoBehaviour
+{
+	float moveSpeed = 10f;
+
+	void FixedUpdate()
+	{
+		transform.Translate(Vector3.right * moveSpeed * Time.fixedDeltaTime);
+	}
+}
