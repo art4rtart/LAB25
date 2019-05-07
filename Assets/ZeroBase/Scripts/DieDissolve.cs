1ad57447d6f29fb24e530105a5235abb033a3c03
Choi Woo Jin <chldnwls94@kpu.ac.kr>
2019-05-07 17:18:36 +0900

    Player die motion added
    Player die motion added

---
+++ Assets/ZeroBase/Scripts/DieDissolve.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieDissolve : MonoBehaviour
+{
+	public float spawnEffectTime = 2;
+	public AnimationCurve fadeIn;
+	public float timer = 0;
+	Renderer _renderer;
+
+	int shaderProperty;
+
+	void Start()
+	{
+		shaderProperty = Shader.PropertyToID("_Dissolved");
+		_renderer = GetComponent<Renderer>();
+	}
+
+	void Update()
+	{
+		if (timer < spawnEffectTime)
+		{
+			timer += Time.deltaTime;
+		}
+		_renderer.material.SetFloat(shaderProperty, fadeIn.Evaluate(Mathf.InverseLerp(0, spawnEffectTime, timer)));
+	}
+}
