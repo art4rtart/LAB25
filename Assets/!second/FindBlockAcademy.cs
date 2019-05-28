658cdeb84c6f45a8e2acdf73f9378272b6629292
wj-choi <sq7r9760@naver.com>
2019-05-29 03:26:37 +0900

    item finding & pathfinding machin learning agent added
    item finding & pathfinding machin learning agent added

---
+++ Assets/!second/FindBlockAcademy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class FindBlockAcademy : Academy
+{
+	public float agentRunSpeed;
+	public float agentRotationSpeed;
+
+	public float spawnAreaMarginMultiplier;
+
+	public Material goalScoredMaterial;
+
+	public Material failMaterial;
+
+	public float gravityMultiplier;
+
+	void State()
+	{
+		Physics.gravity *= gravityMultiplier;
+	}
+}
---
+++ Assets/!second/PathDetect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathDetect : MonoBehaviour
+{
+	public PathFinder pathFinder;
+
+	void Start()
+	{
+		this.gameObject.transform.position = pathFinder.destinations[pathFinder.destinationsIndex] + Vector3.up * 1f;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("agent"))
+		{
+			if (pathFinder.destinationsIndex < pathFinder.maxIndex - 1)
+			{
+				pathFinder.destinationsIndex++;
+				this.gameObject.transform.position = pathFinder.destinations[pathFinder.destinationsIndex] + Vector3.up * 1f;
+			}
+			else
+			{
+				this.gameObject.SetActive(false);
+				other.gameObject.GetComponent<Her0inAgent>().agentRunSpeed = 0;
+			}
+		}
+	}
+}
