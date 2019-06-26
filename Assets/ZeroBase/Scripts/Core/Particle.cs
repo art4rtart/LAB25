0cdc375327088e6b251a167eb29261513e3ad5ea
KHD <all_family@naver.com>
2019-06-26 13:55:27 +0900

    Complete Change Script Structure

---
+++ Assets/ZeroBase/Scripts/Core/Particle.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public static Particle Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+            instance = FindObjectOfType<Particle>();
+            return instance;
+        }
+    }
+
+    private static Particle instance;
+
+    // Prefabs
+    public GameObject hitSparkPrefab;
+    public GameObject hitHolePrefab;
+    public GameObject bulletCasingPrefab;
+    public GameObject bloodParticlePrefab;
+
+    // ObjectPool
+    public static MemoryPool bulletHolePool = new MemoryPool();
+    public static MemoryPool flarePool = new MemoryPool();
+    public static MemoryPool bulletCasingPool = new MemoryPool();
+    public static MemoryPool bloodParticlePool = new MemoryPool();
+
+    private Transform fireTraceParent;
+    public Transform bulletCasingPoint;
+
+    private void Awake()
+    {
+        fireTraceParent = GameObject.Find("ObjectManager").transform;
+    }
+
+    private void Start()
+    {
+        bulletHolePool.Create(hitHolePrefab, 6, fireTraceParent);
+        flarePool.Create(hitSparkPrefab, 6, fireTraceParent);
+        bulletCasingPool.Create(bulletCasingPrefab, 11, fireTraceParent);
+        bloodParticlePool.Create(bloodParticlePrefab, 100, fireTraceParent);
+    }
+    public IEnumerator BulletEffect()
+    {
+        GameObject bulletCasingObject;
+        bulletCasingObject = bulletCasingPool.NewItem();
+        Quaternion randomQuaternion = new Quaternion(Random.Range(0, 360f), Random.Range(0, 360f), Random.Range(180, 360f), 1);
+
+        if (bulletCasingObject)
+        {
+            bulletCasingObject.transform.position = transform.TransformPoint(transform.InverseTransformPoint(bulletCasingPoint.position) + new Vector3(0.4f, 0, 0.3f));
+            bulletCasingObject.transform.localScale = new Vector3(2, 2, 2);
+            bulletCasingObject.transform.SetParent(fireTraceParent);
+            bulletCasingObject.transform.localRotation = randomQuaternion;
+            //bulletCasingObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(0.01f, 0.05f), Random.Range(0.01f, 0.05f), Random.Range(-0.03f, 0.03f)));
+            bulletCasingObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(50f, 100f), Random.Range(50f, 100f), Random.Range(-30f, 30f)));
+        }
+        yield return new WaitForSeconds(1f);
+
+        bulletCasingPool.RemoveItem(bulletCasingObject, bulletCasingPrefab, fireTraceParent);
+    }
+
+    public IEnumerator FireEffect(Vector3 pos, Quaternion rot)
+    {
+        GameObject bulletHole, flare;
+        bulletHole = bulletHolePool.NewItem();
+        flare = flarePool.NewItem();
+
+        if (bulletHole)
+        {
+            bulletHole.transform.position = pos;
+            bulletHole.transform.SetParent(fireTraceParent);
+            bulletHole.transform.rotation = rot;
+
+            flare.transform.position = pos;
+            flare.transform.SetParent(fireTraceParent);
+            flare.transform.rotation = rot;
+        }
+        yield return new WaitForSeconds(0.5f);
+        flarePool.RemoveItem(flare, hitSparkPrefab, fireTraceParent);
+
+        yield return new WaitForSeconds(0.5f);
+        bulletHolePool.RemoveItem(bulletHole, hitHolePrefab, fireTraceParent);
+    }
+
+    public IEnumerator BloodEffect(Vector3 pos)
+    {
+        GameObject bloodParticleTemp;
+        bloodParticleTemp = bloodParticlePool.NewItem();
+        if (bloodParticleTemp)
+        {
+            bloodParticleTemp.transform.position = pos;
+            bloodParticleTemp.transform.SetParent(fireTraceParent);
+        }
+        yield return new WaitForSeconds(1.0f);
+        bloodParticlePool.RemoveItem(bloodParticleTemp, bloodParticlePrefab, fireTraceParent);
+    }
+
+    void OnApplicationQuit()
+    {
+        bloodParticlePool.Dispose();
+        bulletHolePool.Dispose();
+        flarePool.Dispose();
+        bulletCasingPool.Dispose();
+    }
+}
--- Assets/ZeroBase/Scripts/Player/WeaponCtrl.cs
+++ Assets/ZeroBase/Scripts/Player/WeaponCtrl.cs
@@ -12,16 +12,13 @@ public class WeaponCtrl : MonoBehaviour
     public static int currentBullets = 30;
     public float range;
     public float fireRate;
-    private Vector3 originalPos;
     public float accuracy;
-    private float originalAccuracy;
     public int damage;
 
     // Parameters
     private float fireTimer;
     public bool isReloaded;
     private bool isReloading;
-    private bool isAiming;
     private bool isRunning;
 
     // Sounds
@@ -39,23 +36,9 @@ public class WeaponCtrl : MonoBehaviour
     public Transform shootPoint;
     private Animator anim;
     public ParticleSystem muzzleFlash;
-    public Transform bulletCasingPoint;
     private CharacterController characterController;
 
-    // Prefabs
-    public GameObject hitSparkPrefab;
-    public GameObject hitHolePrefab;
-    public GameObject bulletCasingPrefab;
-    public GameObject bloodParticlePrefab;
-
-    // ObjectPool
-    public static MemoryPool bulletHolePool = new MemoryPool();
-    public static MemoryPool flarePool = new MemoryPool();
-    public static MemoryPool bulletCasingPool = new MemoryPool();
-    public static MemoryPool bloodParticlePool = new MemoryPool();
-
     private int useWard = 0;
-    private Transform fireTraceParent;
 
 	// her0in
 	public int bulletsToReload;
@@ -67,16 +50,9 @@ public class WeaponCtrl : MonoBehaviour
 	int specialItemIndex;
 	public MissionScripts missionScripts;
 
-	private void Awake()
-    {
-        fireTraceParent = GameObject.Find("ObjectManager").transform;
-    }
+	
     private void Start()
     {
-        bulletHolePool.Create(hitHolePrefab, 6, fireTraceParent);
-        flarePool.Create(hitSparkPrefab, 6, fireTraceParent);
-        bulletCasingPool.Create(bulletCasingPrefab, 11, fireTraceParent);
-        bloodParticlePool.Create(bloodParticlePrefab, 100, fireTraceParent);
         characterController = GetComponentInParent<CharacterController>();
 
         anim = GameObject.Find("Player").GetComponent<Animator>();
@@ -211,14 +187,14 @@ public class WeaponCtrl : MonoBehaviour
 
             if (!hit.transform.gameObject.CompareTag("Infectee") && !hit.transform.gameObject.CompareTag("Player") && !hit.transform.gameObject.CompareTag("PlayerAgent") && !hit.transform.gameObject.CompareTag("Untagged"))
             {
-				StartCoroutine(FireEffect(hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal)));
+				StartCoroutine(Particle.Instance.FireEffect(hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal)));
 			}
 			else
 			{
 				if (health && health.hp > 0)
 				{
                     health.ApplyDamage(damage, hit.transform.InverseTransformPoint(hit.point));
-					StartCoroutine(BloodEffect(hit.point));
+					StartCoroutine(Particle.Instance.BloodEffect(hit.point));
 				}
             }
         }
@@ -228,74 +204,7 @@ public class WeaponCtrl : MonoBehaviour
         audioSource.PlayOneShot(shootSound);    //shoot sound
         muzzleFlash.Play();
         Recoil();
-        StartCoroutine(BulletEffect());
-    }
-    //void BulletEffect()
-    //{ 
-    //    Quaternion randomQuaternion = new Quaternion(Random.Range(0, 360f), Random.Range(0, 360f), Random.Range(180, 360f), 1);
-
-
-    //    GameObject casing = Instantiate(bulletCasing, bulletCasingPoint);
-    //    casing.transform.localRotation = randomQuaternion;
-    //    casing.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(50f, 100f), Random.Range(50f, 100f), Random.Range(-30f, 30f)));
-    //    Destroy(casing, 1f);
-    //}
-
-    IEnumerator BloodEffect(Vector3 pos)
-    {
-        GameObject bloodParticleTemp;
-        bloodParticleTemp = bloodParticlePool.NewItem();
-        if (bloodParticleTemp)
-        {
-            bloodParticleTemp.transform.position = pos;
-            bloodParticleTemp.transform.SetParent(fireTraceParent);
-        }
-        yield return new WaitForSeconds(1.0f);
-        bloodParticlePool.RemoveItem(bloodParticleTemp, bloodParticlePrefab, fireTraceParent);
-    }
-
-    IEnumerator FireEffect(Vector3 pos, Quaternion rot)
-    {
-        GameObject bulletHole, flare;
-        bulletHole = bulletHolePool.NewItem();
-        flare = flarePool.NewItem();
-
-        if (bulletHole)
-        {
-            bulletHole.transform.position = pos;
-            bulletHole.transform.SetParent(fireTraceParent);
-            bulletHole.transform.rotation = rot;
-
-            flare.transform.position = pos;
-            flare.transform.SetParent(fireTraceParent);
-            flare.transform.rotation = rot;
-        }
-        yield return new WaitForSeconds(0.5f);
-        flarePool.RemoveItem(flare, hitSparkPrefab, fireTraceParent);
-
-        yield return new WaitForSeconds(0.5f);
-        bulletHolePool.RemoveItem(bulletHole, hitHolePrefab, fireTraceParent);
-    }
-
-    IEnumerator BulletEffect()
-    {
-        GameObject bulletCasingObject;
-        bulletCasingObject = bulletCasingPool.NewItem();
-        Quaternion randomQuaternion = new Quaternion(Random.Range(0, 360f), Random.Range(0, 360f), Random.Range(180, 360f), 1);
-
-        if (bulletCasingObject)
-        {
-            bulletCasingObject.transform.position = transform.TransformPoint(transform.InverseTransformPoint(bulletCasingPoint.position) + new Vector3(0.4f, 0, 0.3f));
-            bulletCasingObject.transform.localScale = new Vector3(2, 2, 2);
-            bulletCasingObject.transform.SetParent(fireTraceParent);
-            bulletCasingObject.transform.localRotation = randomQuaternion;
-            //bulletCasingObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(0.01f, 0.05f), Random.Range(0.01f, 0.05f), Random.Range(-0.03f, 0.03f)));
-            bulletCasingObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(50f, 100f), Random.Range(50f, 100f), Random.Range(-30f, 30f)));
-        }
-        yield return new WaitForSeconds(1f);
-
-        bulletCasingPool.RemoveItem(bulletCasingObject, bulletCasingPrefab, fireTraceParent);
-
+        StartCoroutine(Particle.Instance.BulletEffect());
     }
 
     private void Recoil()
@@ -334,13 +243,5 @@ public class WeaponCtrl : MonoBehaviour
         bulletsTotal -= bulletsToReload;
     }
 
-
-    void OnApplicationQuit()
-    {
-        bloodParticlePool.Dispose();
-        bulletHolePool.Dispose();
-        flarePool.Dispose();
-        bulletCasingPool.Dispose();
-    }
 }
 
